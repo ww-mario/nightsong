5ccb08dc17f5d1059cbc4de7235ef6f98132e37d
Elmario Husha <elmario.husha@reachplc.com>
2020-12-16 06:05:51 +0000

    Add mail

--- public/bootstrap/js/scripts.js
+++ public/bootstrap/js/scripts.js
@@ -31,4 +31,36 @@
        $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
     });
 
+    $('#send-mail').click(function() {
+        $('#alertModal > div > div > div > h2').html('Sending');
+        $('#alertModal > div > div > div > p').html('Please wait...');
+        var company = $('input[name="company"]').val();
+        var email = $('input[name="email"]').val();
+        var phone = $('input[name="phone"]').val();
+        var text = $('textarea[name="text"]').val();
+        if(text.length < 5 || (phone.length < 1 || email.length < 1)) {
+            $('#alertModal > div > div > div > h2').html('Error');
+            $('#alertModal > div > div > div > p').html('Please fill in the entire form');
+        } else {
+            var obj = {
+                company: company,
+                email: email,
+                phone: phone,
+                text: text
+            };
+            
+            $.post('/mail', obj, function() {
+                $('#alertModal > div > div > div > h2').html('Message Sent');
+                $('#alertModal > div > div > div > p').html('Thank you for your inquiry, we\'ll get back to you as soon as possible');
+
+                $('input[name="company"]').val('');
+                $('input[name="email"]').val('');
+                $('input[name="phone"]').val('');
+                $('textarea[name="text"]').val('');
+            });
+            
+        }
+
+    });
+
 })(jQuery);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+var nodemailer = require('nodemailer');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/mail', function(req, res, next) {
+  const company = req.body.company;
+  const email = req.body.email;
+  const text = req.body.text;
+  const phone = req.body.phone;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'elmario@nightsong.digital',
+      pass: 'Husha6129!'
+    }
+  });
+  
+  const mailOptions = {
+    from: 'elmario@nightsong.digital',
+    to: 'elmario@nightsong.digital',
+    subject: 'New Website Message - ' + company,
+    html: '<b>Name/Company:</b> ' + company + ' <br /><b>Email: </b> ' + email + ' <br /> <b>Phone: </b>' + phone + '<br /> <b>Text:</b> ' + text
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+      res.send(500);
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send(200);
+    }
+  });
+  
 });
 
 module.exports = router;
